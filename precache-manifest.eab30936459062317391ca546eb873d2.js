04188db93377aeba618858d4f50d4d138c166f18
Njabulo Nsibande <njabsoul@gmail.com>
2019-06-23 08:20:26 +0200

    Updates

---
+++ precache-manifest.eab30936459062317391ca546eb873d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e275bbff2be1c75de26d2f186e9bf8fd",
+    "url": "/income_tax_calc/index.html"
+  },
+  {
+    "revision": "075494c6c4640c9c382b",
+    "url": "/income_tax_calc/static/css/main.72a172cd.chunk.css"
+  },
+  {
+    "revision": "974403bcf4252477dcc7",
+    "url": "/income_tax_calc/static/js/2.a0aafeea.chunk.js"
+  },
+  {
+    "revision": "075494c6c4640c9c382b",
+    "url": "/income_tax_calc/static/js/main.14209910.chunk.js"
+  },
+  {
+    "revision": "4bd8e4f7c5a7f089dfc4",
+    "url": "/income_tax_calc/static/js/runtime~main.3642bb06.js"
+  }
+]);
